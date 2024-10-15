44ce74993ea3fbdb1ed02f99f95e55450f234e58
Alexandre Amice <43739209+AlexandreAmice@users.noreply.github.com>
2024-10-15 13:28:01 -0400

    A more flexible implementation of IrisFromCliqueCover (#9)

---
+++ planning/iris/iris_from_clique_cover_v2.cc
@@ -0,0 +1,91 @@
+#include "drake/planning/iris/iris_from_clique_cover_v2.h"
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <vector>
+
+#include "drake/planning/graph_algorithms/max_clique_solver_base.h"
+#include "drake/planning/graph_algorithms/max_clique_solver_via_greedy.h"
+#include "drake/planning/graph_algorithms/max_clique_solver_via_mip.h"
+#include "drake/planning/graph_algorithms/min_clique_cover_solver_via_greedy.h"
+#include "drake/planning/iris/hpolyhedron_point_sampler.h"
+#include "drake/planning/iris/iris_in_configuration_space_clique_inflation.h"
+
+namespace drake {
+namespace planning {
+namespace iris {
+using geometry::optimization::HPolyhedron;
+using geometry::optimization::Hyperellipsoid;
+namespace {
+std::unique_ptr<planning::graph_algorithms::MaxCliqueSolverBase>
+MakeDefaultMaxCliqueSolver() {
+  return std::unique_ptr<planning::graph_algorithms::MaxCliqueSolverBase>(
+      new planning::graph_algorithms::MaxCliqueSolverViaGreedy());
+}
+
+}  // namespace
+
+void IrisInConfigurationSpaceFromCliqueCoverV2(
+    const CollisionChecker& checker, const IrisFromCliqueCoverOptions& options,
+    RandomGenerator* generator, std::vector<HPolyhedron>* sets,
+    const planning::graph_algorithms::MaxCliqueSolverBase*
+        max_clique_solver_ptr) {
+  DRAKE_THROW_UNLESS(options.coverage_termination_threshold > 0);
+  DRAKE_THROW_UNLESS(options.iteration_limit > 0);
+
+  // Note: Even though the iris_options.bounding_region may be provided,
+  // IrisInConfigurationSpace (currently) requires finite joint limits.
+  DRAKE_THROW_UNLESS(
+      checker.plant().GetPositionLowerLimits().array().isFinite().all());
+  DRAKE_THROW_UNLESS(
+      checker.plant().GetPositionUpperLimits().array().isFinite().all());
+
+  const HPolyhedron domain = options.iris_options.bounding_region.value_or(
+      HPolyhedron::MakeBox(checker.plant().GetPositionLowerLimits(),
+                           checker.plant().GetPositionUpperLimits()));
+  DRAKE_THROW_UNLESS(domain.ambient_dimension() ==
+                     checker.plant().num_positions());
+
+  std::unique_ptr<planning::graph_algorithms::MaxCliqueSolverBase>
+      default_max_clique_solver;
+  // Only construct the default solver if max_clique_solver is null.
+  if (max_clique_solver_ptr == nullptr) {
+    std::cout << "making default max clique solver" << std::endl;
+    default_max_clique_solver = MakeDefaultMaxCliqueSolver();
+    log()->info("Using default max clique solver MaxCliqueSolverViaGreedy.");
+  }
+  std::cout << "made default max clique solver" << std::endl;
+
+  const graph_algorithms::MaxCliqueSolverBase* max_clique_solver =
+      max_clique_solver_ptr == nullptr ? default_max_clique_solver.get()
+                                       : max_clique_solver_ptr;
+  std::cout << "max clique solver made" << std::endl;
+
+  // Override options which are set too aggressively.
+  const int minimum_clique_size = std::max(options.minimum_clique_size,
+                                           checker.plant().num_positions() + 1);
+  graph_algorithms::MinCliqueCoverSolverViaGreedy min_clique_cover_solver(
+      *max_clique_solver, minimum_clique_size);
+  std::cout << "min clique solver made" << std::endl;
+
+  std::unique_ptr<PointSamplerBase> point_sampler =
+      std::make_unique<HPolyhedronPointSampler>(domain);
+  std::cout << "domain sampler made" << std::endl;
+
+  std::unique_ptr<RegionFromCliqueBase> set_builder =
+      std::make_unique<IrisInConfigurationSpaceCliqueInflation>(
+          checker, options.iris_options,
+          options.rank_tol_for_minimum_volume_circumscribed_ellipsoid);
+  std::cout << "set builders made sampler made" << std::endl;
+
+  IrisInConfigurationSpaceFromCliqueCoverTemplate(
+      options, checker, generator, point_sampler.get(),
+      &min_clique_cover_solver, set_builder.get(), sets);
+  std::cout << "VCC V2 done." << std::endl;
+}
+
+}  // namespace iris
+}  // namespace planning
+}  // namespace drake
---
+++ planning/iris/test/box_in_corner_test_fixture.h
@@ -0,0 +1,202 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/common/ssize.h"
+#include "drake/geometry/optimization/hpolyhedron.h"
+#include "drake/geometry/optimization/hyperrectangle.h"
+#include "drake/geometry/optimization/vpolytope.h"
+#include "drake/geometry/test_utilities/meshcat_environment.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/planning/iris/hpolyhedron_point_sampler.h"
+#include "drake/planning/iris/iris_from_clique_cover_options.h"
+#include "drake/planning/robot_diagram_builder.h"
+#include "drake/planning/scene_graph_collision_checker.h"
+#include "drake/systems/framework/diagram_builder.h"
+namespace drake {
+namespace planning {
+namespace iris {
+
+// Draw a two dimensional polytope in meshcat.
+void Draw2dVPolytope(const geometry::optimization::VPolytope& polytope,
+                     const std::string& meshcat_name,
+                     const Eigen::Ref<const Eigen::Vector3d>& color,
+                     std::shared_ptr<geometry::Meshcat> meshcat);
+// Draws two dimensinoal points to meshcat as spheres.
+void Draw2dPointsToMeshcat(const Eigen::Ref<const Eigen::Matrix2Xd>& points,
+                           std::shared_ptr<geometry::Meshcat> meshcat,
+                           std::string meshcat_name = "points_",
+                           double sphere_size = 0.1,
+                           geometry::Rgba rgba = geometry::Rgba(0.1, 0.1, 0.1,
+                                                                1.0));
+
+const char boxes_in_corners[] = R"""(
+<robot name="boxes">
+  <link name="fixed">
+    <collision name="top_left">
+      <origin rpy="0 0 0" xyz="-1 1 0"/>
+      <geometry><box size="1.4 1.4 1.4"/></geometry>
+    </collision>
+    <collision name="top_right">
+      <origin rpy="0 0 0" xyz="1 1 0"/>
+      <geometry><box size="1.4 1.4 1.4"/></geometry>
+    </collision>
+    <collision name="bottom_left">
+      <origin rpy="0 0 0" xyz="-1 -1 0"/>
+      <geometry><box size="1.4 1.4 1.4"/></geometry>
+    </collision>
+    <collision name="bottom_right">
+      <origin rpy="0 0 0" xyz="1 -1 0"/>
+      <geometry><box size="1.4 1.4 1.4"/></geometry>
+    </collision>
+  </link>
+  <joint name="fixed_link_weld" type="fixed">
+    <parent link="world"/>
+    <child link="fixed"/>
+  </joint>
+  <link name="movable">
+    <collision name="sphere">
+      <geometry><sphere radius="0.01"/></geometry>
+    </collision>
+  </link>
+  <link name="for_joint"/>
+  <joint name="x" type="prismatic">
+    <axis xyz="1 0 0"/>
+    <limit lower="-2" upper="2"/>
+    <parent link="world"/>
+    <child link="for_joint"/>
+  </joint>
+  <joint name="y" type="prismatic">
+    <axis xyz="0 1 0"/>
+    <limit lower="-2" upper="2"/>
+    <parent link="for_joint"/>
+    <child link="movable"/>
+  </joint>
+</robot>
+)""";
+/* A movable sphere with fixed boxes in all corners.
+┌───────────────┐
+│┌────┐   ┌────┐│
+││    │   │    ││
+│└────┘   └────┘│
+│       o       │
+│┌────┐   ┌────┐│
+││    │   │    ││
+│└────┘   └────┘│
+└───────────────┘ */
+class BoxInCornerTestFixture : public ::testing::Test {
+ public:
+  Eigen::MatrixXd SamplePointsFromManualDecompositionPiece(
+      int num_points, int piece_number, RandomGenerator* rng,
+      int mixing_steps = 10) {
+    DRAKE_THROW_UNLESS(piece_number >= 0 && piece_number < 6);
+    manual_decomposition_samplers[piece_number].set_mixing_steps(mixing_steps);
+    return manual_decomposition_samplers[piece_number].SamplePoints(num_points,
+                                                                    rng);
+  }
+
+ protected:
+  void SetUp() override {
+    params = CollisionCheckerParams();
+
+    meshcat = geometry::GetTestEnvironmentMeshcat();
+    meshcat->Delete("/drake");
+    //    meshcat->Set2dRenderMode(math::RigidTransformd(Eigen::Vector3d{0, 0,
+    //    1}),
+    //                             -3.25, 3.25, -3.25, 3.25);
+    meshcat->SetProperty("/Grid", "visible", true);
+    // Draw the true cspace.
+    Eigen::Matrix3Xd env_points(3, 5);
+    // clang-format off
+    env_points << -2, 2,  2, -2, -2,
+                   2, 2, -2, -2,  2,
+                   0, 0,  0,  0,  0;
+    // clang-format on
+    meshcat->SetLine("Domain", env_points, 8.0, geometry::Rgba(0, 0, 0));
+    Eigen::Matrix3Xd centers(3, 4);
+    double c = 1.0;
+    // clang-format off
+    centers << -c, c,  c, -c,
+                c, c, -c, -c,
+                0, 0,  0,  0;
+    // clang-format on
+    Eigen::Matrix3Xd obs_points(3, 5);
+    // approximating offset due to sphere radius with fixed offset
+    double s = 0.7 + 0.01;
+    // clang-format off
+    obs_points << -s, s,  s, -s, -s,
+                   s, s, -s, -s,  s,
+                   s, 0,  0,  0,  0;
+    // clang-format on
+    for (int obstacle_idx = 0; obstacle_idx < 4; ++obstacle_idx) {
+      Eigen::Matrix3Xd obstacle = obs_points;
+      obstacle.colwise() += centers.col(obstacle_idx);
+      meshcat->SetLine(fmt::format("/obstacles/obs_{}", obstacle_idx), obstacle,
+                       8.0, geometry::Rgba(0, 0, 0));
+    }
+
+    RobotDiagramBuilder<double> builder(0.0);
+    params.robot_model_instances =
+        builder.parser().AddModelsFromString(boxes_in_corners, "urdf");
+    params.edge_step_size = 0.01;
+
+    params.model = builder.Build();
+    checker = std::make_unique<SceneGraphCollisionChecker>(std::move(params));
+    options.iris_options.meshcat = meshcat;
+
+    options.num_points_per_coverage_check = 1000;
+    options.num_points_per_visibility_round = 140;
+    options.coverage_termination_threshold = 0.9;
+    options.minimum_clique_size = 4;
+
+    generator = RandomGenerator(0);
+
+    // A manual convex decomposition of the space.
+    manual_decomposition.push_back(geometry::optimization::Hyperrectangle(
+        Eigen::Vector2d{-2, -2}, Eigen::Vector2d{-1.7, 2}));
+    manual_decomposition.push_back(geometry::optimization::Hyperrectangle(
+        Eigen::Vector2d{-2, -2}, Eigen::Vector2d{2, -1.7}));
+    manual_decomposition.push_back(geometry::optimization::Hyperrectangle(
+        Eigen::Vector2d{1.7, -2}, Eigen::Vector2d{2, 2}));
+    manual_decomposition.push_back(geometry::optimization::Hyperrectangle(
+        Eigen::Vector2d{-2, 1.7}, Eigen::Vector2d{2, 2}));
+    manual_decomposition.push_back(geometry::optimization::Hyperrectangle(
+        Eigen::Vector2d{-0.3, -2}, Eigen::Vector2d{0.3, 2}));
+    manual_decomposition.push_back(geometry::optimization::Hyperrectangle(
+        Eigen::Vector2d{-2, -0.3}, Eigen::Vector2d{2, 0.3}));
+
+    color = Eigen::VectorXd::Zero(3);
+    // Show the manual decomposition in the meshcat debugger.
+    for (int i = 0; i < ssize(manual_decomposition); ++i) {
+      // Choose a random color.
+      for (int j = 0; j < color.size(); ++j) {
+        color[j] = abs(gaussian(generator));
+      }
+      color.normalize();
+      geometry::optimization::VPolytope vregion =
+          geometry::optimization::VPolytope(
+              manual_decomposition.at(i).MakeHPolyhedron())
+              .GetMinimalRepresentation();
+      Draw2dVPolytope(vregion, fmt::format("manual_decomposition_{}", i), color,
+                      meshcat);
+    }
+  }
+
+  CollisionCheckerParams params;
+  std::shared_ptr<geometry::Meshcat> meshcat;
+  std::unique_ptr<SceneGraphCollisionChecker> checker;
+  IrisFromCliqueCoverOptions options;
+  RandomGenerator generator;
+  std::vector<geometry::optimization::Hyperrectangle> manual_decomposition;
+  std::vector<HPolyhedronPointSampler> manual_decomposition_samplers;
+  std::normal_distribution<double> gaussian;
+  Eigen::VectorXd color;
+};
+}  // namespace iris
+}  // namespace planning
+}  // namespace drake
